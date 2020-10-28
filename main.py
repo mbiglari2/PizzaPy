f745f943223c6a4b8d0bda3cb4cf3a82cb2d36d3
Biglari_Mazda <MBiglari@hormel.com>
2020-10-28 18:49:59 -0500

    Pizzapi first test
    looked at the api, made some changes, and generated some results

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 from pizzapi import *
+
+#Objects exists in a class
+
+#Classes examples:
+#Customer is a class
+#Object examples: my_store and my_menu
+
+customer = Customer('Blade', 'Nelson', 'powderblock@gmail.com', '4157588103')
+address = Address('75 Westover Ave', 'Ann Arbor', "mi", "48104")
+
+my_store = address.closest_store()
+
+my_menu = my_store.get_menu()
+
+result = my_menu.search(Name = 'Meat')
+
+print(result)
